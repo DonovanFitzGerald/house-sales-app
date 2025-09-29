9cc0a528781efe261902a770fdce0abada499924
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-09-29 13:37:42 +0100

    House table columns

--- database/migrations/2025_09_29_121853_create_houses_table.php
+++ database/migrations/2025_09_29_121853_create_houses_table.php
@@ -14,6 +14,18 @@ return new class extends Migration
         Schema::create('houses', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('description');
+            $table->text('address_line_1');
+            $table->text('address_line_2');
+            $table->text('city');
+            $table->text('county');
+            $table->text('zip');
+            $table->integer('beds');
+            $table->integer('baths');
+            $table->integer('square_metres');
+            $table->enum('energy_rating', ['A1', 'A2', 'A3', 'B1', 'B2', 'B3',  'C1', 'C2', 'C3', 'D1', 'D2', 'E1', 'E2', 'F', 'G']);
+            $table->enum('house_type', ['detatched', 'semi-detached', 'terraced', 'bungalow', 'apartment', 'studio']);
+            $table->integer('featured_image');
         });
     }
 
