0f0f87dc29c8542a622f4126c3b88bb0e6b5d520
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-09-29 14:33:08 +0100

    Plant route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HouseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/houses', [HouseController::class, 'index'] );
