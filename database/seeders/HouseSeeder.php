9d8ce587b3c393e6a596b25d84d61e37b1ce73bc
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-09-29 14:26:45 +0100

    Seeder for houses

--- database/seeders/HouseSeeder.php
+++ database/seeders/HouseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class HouseSeeder extends Seeder
 {
@@ -12,6 +12,37 @@ class HouseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = fake();
+
+        $counties = [
+            'Antrim','Armagh','Carlow','Cavan','Clare','Cork','Derry','Donegal','Down','Dublin',
+            'Fermanagh','Galway','Kerry','Kildare','Kilkenny','Laois','Leitrim','Limerick','Longford',
+            'Louth','Mayo','Meath','Monaghan','Offaly','Roscommon','Sligo','Tipperary','Tyrone',
+            'Waterford','Westmeath','Wexford','Wicklow'
+        ];
+        $energyRatings = ['A1','A2','A3','B1','B2','B3','C1','C2','C3','D1','D2','E1','E2','F','G'];
+        $houseTypes = ['detatched','semi-detached','terraced','bungalow','apartment','studio'];
+
+        $rows = [];
+        for ($i = 0; $i < 100; $i++) {
+            $rows[] = [
+                'description'     => $faker->paragraphs(2, true),
+                'address_line_1'  => $faker->buildingNumber() . ' ' . $faker->streetName(),
+                'address_line_2'  => $faker->secondaryAddress(),
+                'city'            => $faker->city(),
+                'county'          => $faker->randomElement($counties),
+                'zip'             => $faker->postcode(),
+                'beds'            => $faker->numberBetween(1, 6),
+                'baths'           => $faker->numberBetween(1, 4),
+                'square_metres'   => $faker->numberBetween(35, 300),
+                'energy_rating'   => $faker->randomElement($energyRatings),
+                'house_type'      => $faker->randomElement($houseTypes),
+                'featured_image'  => $faker->numberBetween(1, 1000),
+                'created_at'      => now(),
+                'updated_at'      => now(),
+            ];
+        }
+
+        DB::table('houses')->insert($rows);
     }
 }
