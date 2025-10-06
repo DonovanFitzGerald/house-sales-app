a150d1751d784a22533df4cde512c6d974fd02fd
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-10-06 13:22:57 +0100

    Houses nav link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,6 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('houses.index')" :active="request()->routeIs('houses.index')">
+                        {{ __('Houses') }}
+                    </x-nav-link>
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
