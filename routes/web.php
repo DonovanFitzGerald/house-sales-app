05b09c08044354c214786b988578bbc270b89f3c
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-10-25 00:28:29 +0100

    Auth for protected routes

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/houses/create', [HouseController::class, 'create'])->name('houses.create');
+    Route::post('/houses', [HouseController::class, 'store'])->name('houses.store');
+    Route::get('/houses/{house}/edit', [HouseController::class, 'edit'])->name('houses.edit');
+    Route::put('/houses/{house}', [HouseController::class, 'update'])->name('houses.update');
+    Route::delete('/houses/{house}', [HouseController::class, 'destroy'])->name('houses.destroy');
 });
 
 Route::get('/houses', [HouseController::class, 'index'])->name('houses.index');
-Route::get('/houses/create', [HouseController::class, 'create'])->name('houses.create');
 Route::get('/houses/{house}', [HouseController::class, 'show'])->name('houses.show');
-Route::post('/houses', [HouseController::class, 'store'])->name('houses.store');
-Route::get('/houses/{house}/edit', [HouseController::class, 'edit'])->name('houses.edit');
-Route::put('/houses/{house}', [HouseController::class, 'update'])->name('houses.update');
-Route::delete('/houses/{house}', [HouseController::class, 'destroy'])->name('houses.destroy');
+
 
 require __DIR__.'/auth.php';
