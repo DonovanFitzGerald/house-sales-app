b756d39c14fd7ddd43d8a2b1a857d088531e20b1
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-10-24 23:31:19 +0100

    Removed width constraint

--- resources/views/houses/create.blade.php
+++ resources/views/houses/create.blade.php
@@ -9,9 +9,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <x-house-form action="{{ route('houses.store') }}" method="POST" />
-                </div>
+                <x-house-form action="{{ route('houses.store') }}" method="POST" />
             </div>
         </div>
     </div>
