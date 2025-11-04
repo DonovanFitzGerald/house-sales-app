836b94939195b5b1a8a0371a948f214df76b89b9
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-11-04 22:45:33 +0000

    Added phone number to users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,16 +14,20 @@ class UserSeeder extends Seeder
     public function run(): void
     {
 
+        $faker = fake();
+
         $rows = [
             [
                 'name' => 'Admin',
                 'email' => 'admin@example.com',
+                'phone_number' => $faker->phoneNumber(),
                 'password' => Hash::make('password'),
                 'role' => 'admin',
                 'featured_image' => 'placeholder.jpg',
             ],
             [
                 'name' => 'Donovan',
+                'phone_number' => $faker->phoneNumber(),
                 'email' => 'donovan.fitzg@gmail.com',
                 'password' => Hash::make('password'),
                 'role' => 'user',
@@ -31,14 +35,13 @@ class UserSeeder extends Seeder
             ],
             [
                 'name' => 'Donovan Realtor',
+                'phone_number' => $faker->phoneNumber(),
                 'email' => 'donovan.fitzg@realtor.com',
                 'password' => Hash::make('password'),
                 'role' => 'user',
                 'featured_image' => 'placeholder.jpg',
             ],
         ];
-
-        $faker = fake();
 
         for ($i = 0; $i < 200; $i++) {
             $name = $faker->name();
@@ -47,6 +50,7 @@ class UserSeeder extends Seeder
             $rows[] = [
                 'name' => $name,
                 'email' => $email,
+                'phone_number' => $faker->phoneNumber(),
                 'password' => Hash::make('password'),
                 'role' => 'user',
                 'featured_image' => 'person_'.$faker->numberBetween(1, 10).'.jpg',
@@ -60,6 +64,7 @@ class UserSeeder extends Seeder
             $rows[] = [
                 'name' => $name,
                 'email' => $email,
+                'phone_number' => $faker->phoneNumber(),
                 'password' => Hash::make('password'),
                 'role' => 'realtor',
                 'featured_image' => 'person_'.$faker->numberBetween(1, 10).'.jpg',
