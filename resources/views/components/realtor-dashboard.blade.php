de6da50d85528dfd33eaba8e60e6f5f55c567d61
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-11-09 22:14:22 +0000

    Conditional dashboards

---
+++ resources/views/components/realtor-dashboard.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Realtor Dashboard
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,19 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+            @switch(Auth::user()->role)
+
+            @case('admin')
+            <x-admin-dashboard />
+            @break
+
+            @case('realtor')
+            <x-realtor-dashboard />
+            @break
+
+            @default
+            <x-user-dashboard />
+            @endswitch
         </div>
     </div>
 </x-app-layout>
