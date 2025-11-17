6747974483be9b2ab235bf1990bbe95830adfbfe
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-11-17 14:22:09 +0000

    Route resource methods

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // CRUD Routes
-    Route::get('/houses/create', [HouseController::class, 'create'])->name('houses.create');
-    Route::post('/houses', [HouseController::class, 'store'])->name('houses.store');
-    Route::get('/houses/{house}/edit', [HouseController::class, 'edit'])->name('houses.edit');
-    Route::put('/houses/{house}', [HouseController::class, 'update'])->name('houses.update');
-    Route::delete('/houses/{house}', [HouseController::class, 'destroy'])->name('houses.destroy');
 });
 
-// Houses
-Route::get('/houses', [HouseController::class, 'index'])->name('houses.index');
-Route::get('/houses/{house}', [HouseController::class, 'show'])->name('houses.show');
+Route::resource('houses', HouseController::class);
+Route::resource('realtors', RealtorController::class);
 
-// Realtors
-Route::get('/realtors', [RealtorController::class, 'index'])->name('realtors.index');
-Route::get('/realtors/{realtor}', [RealtorController::class, 'show'])->name('realtors.show');
 
 require __DIR__.'/auth.php';
