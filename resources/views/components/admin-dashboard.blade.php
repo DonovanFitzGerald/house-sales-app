2331b05a49ab70de2cef7710416a9735dc4ab974
Donovan FitzGerald <donovan.fitzg@gmail.com>
2025-12-02 03:14:07 +0000

    Fixed admin dashboard

--- resources/views/components/admin-dashboard.blade.php
+++ resources/views/components/admin-dashboard.blade.php
@@ -1,3 +1,7 @@
-<div>
-    Admin Dashboard
-</div>
+<x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Admin Dashboard') }}
+                </h2>
+            </x-slot>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,16 +2,8 @@
     
 
     @switch(Auth::user()->role)
-    
-    @case('admin')
-            <x-slot name="header">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    {{ __('Admin Dashboard') }}
-                </h2>
-            </x-slot>
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <x-admin-dashboard />
-            </div>
+            @case('admin')
+            <x-admin-dashboard />
             @break
 
             @case('realtor')
